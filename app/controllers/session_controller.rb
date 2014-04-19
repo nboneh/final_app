ad35cc93e0094b99842b243eaa272c9f0462364a
Sean Rivera <rivera.shadowoftruth@gmail.com>
2014-04-19 18:37:10 -0300

    Updated to rails 4

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,8 +6,7 @@ class SessionController < ApplicationController
   		user = User.find_by_email(params[:session][:email].downcase)
   		if user && user.authenticate(params[:session][:password])
 	    	# Sign the user in and redirect to the user's show page.
-	    	sign_in user
-          session[:remember_token]  = user.id
+	     	sign_in user
       		redirect_to newsfeed_path
 	    else
 	    	# Create an error message and re-render the signin form.
