f55f854ddcfe265cc1619f1394ff96c4b71808f6
Sean Rivera <rivera.shadowoftruth@gmail.com>
2014-04-23 16:22:30 -0600

    Added some changes

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,5 @@
 class FriendshipsController < ApplicationController
-    
+
     def find
         name=params[:name]
         if(!name.nil?)
@@ -11,6 +11,14 @@ class FriendshipsController < ApplicationController
     end
 
     def create
+        params[:status] = "pending"
+        params[:sender_id] = current_user.id
+        params.require(:status, :receiver_id,:sender_id)
+        @friend = Friend.new(params[:post])
+        if @friend.save
+            redirect_to newsfeed 
+        end
+
     end
 
     def new
