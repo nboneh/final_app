0ee0542aa769a384c17a32d850fcec1e66a87043
saasbook <saasbook@saasbook.(none)>
2014-04-23 17:52:51 -0300

    Wall partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
     belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_pid'
     belongs_to :receiver, :class_name => 'User', :foreign_key => 'receiver_pid'
   attr_accessible :content
+  validates :content, length: { maximum: 140 }
 end
